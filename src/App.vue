0bedbf49daac05b37e9db90879bccd582974ff58
arnold-ng <61305922+arnold-ng@users.noreply.github.com>
2020-03-14 21:55:46 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -12,19 +12,19 @@ export default {
 </script>
 
 <style>
-#app {
+/* #app {
   padding: 0px;
   margin: 0px;
   
   background-color: white;
-}
+} */
 
 html, body {
-  width: 100%;
+  /* width: 100%;
   padding: 0px;
   margin: 0px;
   overflow-x: hidden;
-  overflow-y:scroll;
+  overflow-y:scroll; */
   scrollbar-width: thin;
 }  
 

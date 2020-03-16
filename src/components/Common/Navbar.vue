18aed539ca1e2dfcd595029cb9fde78254ac51de
arnold-ng <61305922+arnold-ng@users.noreply.github.com>
2020-03-16 21:16:47 +0800

    update

--- src/components/Common/Navbar.vue
+++ src/components/Common/Navbar.vue
@@ -1,19 +1,19 @@
 <template>
   <div>
     <b-navbar toggleable="md" type="light" variant="light" class='navbar fixed-top'>
-          <b-navbar-brand to="/" id="nav">Code.io</b-navbar-brand>
+        <b-navbar-brand to="/" id="nav">Code.io</b-navbar-brand>
           
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav class="ml-auto">
-            <b-nav-item to="/" class='pr-2' >Home</b-nav-item>
-            <b-nav-item to="/about" class='pr-2'>About</b-nav-item>
-            <b-nav-item to="/language" class='pr-2' >Language</b-nav-item>
-            <b-nav-item to="/proglang" class='pr-2'>Module</b-nav-item>
-            <b-nav-item to="/exercise" class='pr-2'>Exercise</b-nav-item>
-            <b-nav-item to="/after" class='pr-2'>Account</b-nav-item>
-            <b-nav-item to="/" class='pr-2'>Logout</b-nav-item>
+          <b-nav-item to="/" class='pr-2'>Home</b-nav-item>
+          <b-nav-item to="/about" class='pr-2'>About</b-nav-item>
+          <b-nav-item to="/language" class='pr-2' >Language</b-nav-item>
+          <b-nav-item to="/proglang" class='pr-2'>Module</b-nav-item>
+          <b-nav-item to="/exercise" class='pr-2'>Exercise</b-nav-item>
+          <b-nav-item to="/after" class='pr-2'>Account</b-nav-item>
+          <b-nav-item to="/" class='pr-2'>Logout</b-nav-item>
         </b-navbar-nav>
       </b-collapse>
       
@@ -35,6 +35,8 @@ export default {
   /* font-size: 16px;; */
 }
 
-
+.nav-item {
+  text-align: right;
+}
 
 </style>
--- src/components/Homepage/Navbar.vue
+++ src/components/Homepage/Navbar.vue
@@ -1,28 +1,21 @@
- <template>
+<template>
 <div>
   <b-navbar toggleable="md" type="light" variant="outline-danger" fixed="top" class='bg-overlay'>
-    <b-navbar-brand to="/" id="nav">Code.io
-    </b-navbar-brand>
+    <b-navbar-brand to="/" id="nav">Code.io</b-navbar-brand>
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
     <b-collapse id="nav-collapse" is-nav>
 
-      <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
 
         <b-nav-item-dropdown text="☰" right id="menu" toggle-class="text-dark" no-caret class="drop">
           <b-dropdown-item to="/about" id="menu">About</b-dropdown-item>
-          <b-dropdown-item to="/test" id="menu">Exercise</b-dropdown-item>
+          <b-dropdown-item to="/exercise" id="menu">Exercise</b-dropdown-item>
           <b-dropdown-item to="/language" id="menu">Language</b-dropdown-item>
-          <b-dropdown-item href="#" id="menu">Account</b-dropdown-item>
-
+          <b-dropdown-item to="/after" id="menu">Account</b-dropdown-item>
         </b-nav-item-dropdown>
 
-        <!-- <b-nav-item-dropdown right text="User" id="menu" toggle-class="text-dark">
-          <b-dropdown-item href="#" id="menu">Sign In</b-dropdown-item>
-          <b-dropdown-item href="#" id="menu">Register</b-dropdown-item>
-        </b-nav-item-dropdown> -->
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>

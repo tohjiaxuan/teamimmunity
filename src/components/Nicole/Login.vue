18486c11b31c6258e1ec993226d7bd50502a26e9
nicolepng <56541613+nicolepng@users.noreply.github.com>
2020-03-17 13:05:03 +0800

    Update Login.vue

--- src/components/Nicole/Login.vue
+++ src/components/Nicole/Login.vue
@@ -101,7 +101,7 @@ export default {
 #box {
     height: 30%;
     width: 60%;
-    margin-top: 20%;
+    margin-top: 15%;
     margin-left: 20%;
     background-color: rgba(196, 196, 196, 0.7);
     box-shadow: 50px;

25fc33a5d6db048760ddcfd58890a8eed29f6bf6
arnold-ng <61305922+arnold-ng@users.noreply.github.com>
2020-03-26 19:46:15 +0800

    increment clicks in firebase

--- src/components/Common/Navbar.vue
+++ src/components/Common/Navbar.vue
@@ -9,14 +9,14 @@
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav class="ml-auto">
-          <b-nav-item to="/" class='pr-2'>Home</b-nav-item>
-          <b-nav-item to="/about" class='pr-2'>About</b-nav-item>
-          <b-nav-item to="/language" class='pr-2' >Language</b-nav-item>
-          <b-nav-item to="/module" class='pr-2'>Module</b-nav-item>
-          <b-nav-item to="/exercise" class='pr-2'>Exercise</b-nav-item>
-          <b-nav-item to="/account" class='pr-2'>Account</b-nav-item>
-          <b-nav-item to="/" class='pr-2'>Logout</b-nav-item>
-          <b-nav-item to="/contact" class='pr-2'>?</b-nav-item>
+          <b-nav-item to="/" class='pr-2' v-on:click='incrementClick("home")'>Home</b-nav-item>
+          <b-nav-item to="/about" class='pr-2' v-on:click='incrementClick("about")'>About</b-nav-item>
+          <b-nav-item to="/language" class='pr-2' v-on:click='incrementClick("language")'>Language</b-nav-item>
+          <b-nav-item to="/module" class='pr-2' v-on:click='incrementClick("module")'>Module</b-nav-item>
+          <b-nav-item to="/exercise" class='pr-2' v-on:click='incrementClick("exercise")'>Exercise</b-nav-item>
+          <b-nav-item to="/account" class='pr-2' v-on:click='incrementClick("account")'>Account</b-nav-item>
+          <b-nav-item to="/" class='pr-2' v-on:click='incrementClick("logout")'>Logout</b-nav-item>
+          <b-nav-item to="/contact" class='pr-2' v-on:click='incrementClick("contact")'>?</b-nav-item>
         </b-navbar-nav>
       </b-collapse>
       
@@ -25,8 +25,9 @@
 </template>
 
 <script>
+import clicks from "../Common/clicks.js"
 export default {
-  
+  mixins: [clicks]
 }
 </script>
 

c9850130d929dbf3d847688132aefa0bb7529687
tohjiaxuan <61282771+tohjiaxuan@users.noreply.github.com>
2020-04-16 00:48:43 +0800

    easy exercises graph

--- src/components/Dashboard/easyrank.js
+++ src/components/Dashboard/easyrank.js
@@ -1,44 +1,19 @@
 import { Bar } from 'vue-chartjs'
 import db from '../../firebase.js'
+import firebase from 'firebase';
 
 export default {
   extends: Bar,
   data: function () {
     return {
-        // datacollection: {
-        //     datasets: []
-        // },
         datacollection: {
           labels: [],
           datasets: [{
-            label: "Completed",
+            label: "No. of hints",
             backgroundColor: [],
             data: []
           }]
         },
-        // options: {
-        //   legend: { display: true},
-        //   title: {
-        //     display: true,
-        //     // text: "Look at other users' interest level in the languages"
-        //   }, 
-        //   scales: {
-        //     yAxes: [{ 
-        //       scaleLabel: {
-        //         display: true,
-        //         labelString: "Number of completed exercises"
-        //       }
-        //     }],
-        //     xAxes: [{ 
-        //       scaleLabel: {
-        //         display: true,
-        //         labelString: "Type of Exercise"
-        //       }
-        //     }]
-        //   },
-        //     responsive: true,
-        //     maintainAspectRatio: false
-        // }
         options: {
           legend: { display: false },
           title: {
@@ -61,29 +36,42 @@ export default {
     }
   },
   methods: {
-    // fetchItems: function () {
-    //   db.collection('language activity').get().then(querySnapShot => {
-    //     querySnapShot.forEach(doc => {
-    //         var newDataset = {
-    //             label: doc.data().language,
-    //             backgroundColor: doc.data().color,
-    //             data: []
-    //         }
-    //         newDataset.data.push(doc.data().clicks)
-    //         this.datacollection.datasets.push(newDataset)
-    //     })
-    //     this.renderChart(this.datacollection, this.options)
-    //   })
-    // }
     fetchItems: function() {
-      db.collection('java exercises activity').get().then(querySnapShot => {
-        querySnapShot.forEach(doc => {
-          this.datacollection.labels.push(doc.data().type)
-          this.datacollection.datasets[0].backgroundColor.push(doc.data().color)
-          this.datacollection.datasets[0].data.push(doc.data().clicks)
+      var user = firebase.auth().currentUser;
+      db.collection('users').doc(user).get().then(snapshot => {
+        const document = snapshot.data()
+        document.collection('easy exercises hint').get().then(querySnapShot => {
+          querySnapShot.forEach(doc => {
+            this.datacollection.labels.push(doc.data().language)
+            this.datacollection.datasets[0].backgroundColor.push(doc.data().color)
+            this.datacollection.datasets[0].data.push(doc.data().hint)
+          })
         })
         this.renderChart(this.datacollection, this.options)
       })
+
+      // db.collection('users').doc(user).get().then(snapshot => {
+      //   const document = snapshot.data()
+      //   document.collection('easy exercises hint').get().then(querySnapShot => {
+      //     querySnapShot.forEach(doc => {
+      //       this.datacollection.labels.push(doc.data().language)
+      //       this.datacollection.datasets[0].backgroundColor.push(doc.data().color)
+      //       this.datacollection.datasets[0].data.push(doc.data().hint)
+      //     })
+      //   })
+      //   this.renderChart(this.datacollection, this.options)
+      // })
+
+      // db.collection('users').collection('easy exercises hint').get().then(querySnapShot => {
+      //   querySnapShot.forEach(doc => {
+      //     if (user.email == doc.data().email) {
+      //       this.datacollection.labels.push(doc.data().language)
+      //       this.datacollection.datasets[0].backgroundColor.push(doc.data().color)
+      //       this.datacollection.datasets[0].data.push(doc.data().hint)
+      //     }
+      //   })
+      //   this.renderChart(this.datacollection, this.options)
+      // })
     }
   },
   created() {

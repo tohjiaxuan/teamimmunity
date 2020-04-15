5e2c31e971f46f0e40344ba00d7460fbbb454b4c
tohjiaxuan <61282771+tohjiaxuan@users.noreply.github.com>
2020-04-15 20:45:12 +0800

    solve undefined part of dashboard

--- src/components/Dashboard/jsexrank.js
+++ src/components/Dashboard/jsexrank.js
@@ -5,54 +5,88 @@ export default {
   extends: Bar,
   data: function () {
     return {
+        // datacollection: {
+        //     datasets: []
+        // },
         datacollection: {
-            datasets: []
+          labels: [],
+          datasets: [{
+            label: "Completed",
+            backgroundColor: [],
+            data: []
+          }]
         },
+        // options: {
+        //   legend: { display: true},
+        //   title: {
+        //     display: true,
+        //     // text: "Look at other users' interest level in the languages"
+        //   }, 
+        //   scales: {
+        //     yAxes: [{ 
+        //       scaleLabel: {
+        //         display: true,
+        //         labelString: "Number of completed exercises"
+        //       }
+        //     }],
+        //     xAxes: [{ 
+        //       scaleLabel: {
+        //         display: true,
+        //         labelString: "Type of Exercise"
+        //       }
+        //     }]
+        //   },
+        //     responsive: true,
+        //     maintainAspectRatio: false
+        // }
         options: {
-          legend: { display: true},
+          legend: { display: false },
           title: {
             display: true,
-            // text: 'Look at the number of Javascript exercises other users have completed'
-          }, 
+            // text: "Look at the number of Javascript exercises other users have completed"
+          },
           scales: {
-            yAxes: [{ 
-              scaleLabel: {
-                display: true,
-                labelString: "Number of completed exercises"
-              },
+            yAxes: [{
               ticks: {
-                beginAtZero: true
+                beginAtZero:true
               }
             }],
-            xAxes: [{ 
-              scaleLabel: {
-                display: true,
-                labelString: "Type of Exercise"
-              }
+            xAxes: [{
+
             }]
           },
-            responsive: true,
-            maintainAspectRatio: false
+          responsive: true,
+          maintainAspectRatio: false
         }
     }
   },
   methods: {
-    fetchItems: function () {
+    // fetchItems: function () {
+    //   db.collection('language activity').get().then(querySnapShot => {
+    //     querySnapShot.forEach(doc => {
+    //         var newDataset = {
+    //             label: doc.data().language,
+    //             backgroundColor: doc.data().color,
+    //             data: []
+    //         }
+    //         newDataset.data.push(doc.data().clicks)
+    //         this.datacollection.datasets.push(newDataset)
+    //     })
+    //     this.renderChart(this.datacollection, this.options)
+    //   })
+    // }
+    fetchItems: function() {
       db.collection('javascript exercises activity').get().then(querySnapShot => {
         querySnapShot.forEach(doc => {
-          var newDataset = {
-              label: doc.data().type,
-              backgroundColor: doc.data().color,
-              data: []
-          }
-          newDataset.data.push(doc.data().clicks)
-          this.datacollection.datasets.push(newDataset)
+          this.datacollection.labels.push(doc.data().type)
+          this.datacollection.datasets[0].backgroundColor.push(doc.data().color)
+          this.datacollection.datasets[0].data.push(doc.data().clicks)
         })
         this.renderChart(this.datacollection, this.options)
       })
     }
   },
-  created () {
+  created() {
     this.fetchItems()
   }
 }

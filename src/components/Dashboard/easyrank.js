ddbc7af7f0a0173c673d53fe8ccbdf5bbf6512f4
tohjiaxuan <61282771+tohjiaxuan@users.noreply.github.com>
2020-04-16 13:59:25 +0800

    update difficulty ranks for dashboard

--- src/components/Dashboard/easyrank.js
+++ src/components/Dashboard/easyrank.js
@@ -7,7 +7,7 @@ export default {
   data: function () {
     return {
         datacollection: {
-          labels: ['java','js','py'],
+          labels: ['Java','Javascript','Python'],
           datasets: [{
             label: "No. of hints",
             backgroundColor: [],
@@ -18,7 +18,7 @@ export default {
           legend: { display: false },
           title: {
             display: true,
-            // text: "Look at the number of Java exercises other users have completed"
+            text: "Difficulty Level: Easy"
           },
           scales: {
             yAxes: [{
@@ -36,52 +36,16 @@ export default {
     }
   },
   methods: {
-    // fetchItems: function() {
-    //   var user = firebase.auth().currentUser;
-    //   db.collection('users').doc(user).get().then(snapshot => {
-    //     const document = snapshot.data()
-    //     document.collection('easy exercises hint').get().then(querySnapShot => {
-    //       querySnapShot.forEach(doc => {
-    //         this.datacollection.labels.push(doc.data().language)
-    //         this.datacollection.datasets[0].backgroundColor.push(doc.data().color)
-    //         this.datacollection.datasets[0].data.push(doc.data().hint)
-    //       })
-    //     })
-    //     this.renderChart(this.datacollection, this.options)
-    //   })
-
-      fetchItems: function() {
-        var user = firebase.auth().currentUser;
-        db.collection('users').doc(user.uid).collection('easy exercises hint').get().then(querySnapshot => {
-          querySnapshot.forEach(doc => {
-            console.log(doc.data().hint)
-            this.datacollection.datasets[0].data.push(doc.data().hint)
-          })
-          this.renderChart(this.datacollection, this.options)
+    fetchItems: function() {
+      var user = firebase.auth().currentUser;
+      db.collection('users').doc(user.uid).collection('easy exercises hint').get().then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+          console.log(doc.data().hint)
+          this.datacollection.datasets[0].backgroundColor.push(doc.data().color)
+          this.datacollection.datasets[0].data.push(doc.data().hint)
         })
-
-      // db.collection('users').doc(user).get().then(snapshot => {
-      //   const document = snapshot.data()
-      //   document.collection('easy exercises hint').get().then(querySnapShot => {
-      //     querySnapShot.forEach(doc => {
-      //       this.datacollection.labels.push(doc.data().language)
-      //       this.datacollection.datasets[0].backgroundColor.push(doc.data().color)
-      //       this.datacollection.datasets[0].data.push(doc.data().hint)
-      //     })
-      //   })
-      //   this.renderChart(this.datacollection, this.options)
-      // })
-
-      // db.collection('users').collection('easy exercises hint').get().then(querySnapShot => {
-      //   querySnapShot.forEach(doc => {
-      //     if (user.email == doc.data().email) {
-      //       this.datacollection.labels.push(doc.data().language)
-      //       this.datacollection.datasets[0].backgroundColor.push(doc.data().color)
-      //       this.datacollection.datasets[0].data.push(doc.data().hint)
-      //     }
-      //   })
-      //   this.renderChart(this.datacollection, this.options)
-      // })
+        this.renderChart(this.datacollection, this.options)
+      })
     }
   },
   created() {
--- src/components/Dashboard/medrank.js
+++ src/components/Dashboard/medrank.js
@@ -1,49 +1,24 @@
 import { Bar } from 'vue-chartjs'
 import db from '../../firebase.js'
+import firebase from 'firebase';
 
 export default {
   extends: Bar,
   data: function () {
     return {
-        // datacollection: {
-        //     datasets: []
-        // },
         datacollection: {
-          labels: [],
+          labels: ['Java','Javascript','Python'],
           datasets: [{
-            label: "Completed",
+            label: "No. of hints",
             backgroundColor: [],
             data: []
           }]
         },
-        // options: {
-        //   legend: { display: true},
-        //   title: {
-        //     display: true,
-        //     // text: "Look at other users' interest level in the languages"
-        //   }, 
-        //   scales: {
-        //     yAxes: [{ 
-        //       scaleLabel: {
-        //         display: true,
-        //         labelString: "Number of completed exercises"
-        //       }
-        //     }],
-        //     xAxes: [{ 
-        //       scaleLabel: {
-        //         display: true,
-        //         labelString: "Type of Exercise"
-        //       }
-        //     }]
-        //   },
-        //     responsive: true,
-        //     maintainAspectRatio: false
-        // }
         options: {
           legend: { display: false },
           title: {
             display: true,
-            // text: "Look at the number of Javascript exercises other users have completed"
+            text: "Difficulty Level: Medium"
           },
           scales: {
             yAxes: [{
@@ -61,26 +36,13 @@ export default {
     }
   },
   methods: {
-    // fetchItems: function () {
-    //   db.collection('language activity').get().then(querySnapShot => {
-    //     querySnapShot.forEach(doc => {
-    //         var newDataset = {
-    //             label: doc.data().language,
-    //             backgroundColor: doc.data().color,
-    //             data: []
-    //         }
-    //         newDataset.data.push(doc.data().clicks)
-    //         this.datacollection.datasets.push(newDataset)
-    //     })
-    //     this.renderChart(this.datacollection, this.options)
-    //   })
-    // }
     fetchItems: function() {
-      db.collection('javascript exercises activity').get().then(querySnapShot => {
-        querySnapShot.forEach(doc => {
-          this.datacollection.labels.push(doc.data().type)
+      var user = firebase.auth().currentUser;
+      db.collection('users').doc(user.uid).collection('medium exercises hint').get().then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+          console.log(doc.data().hint)
           this.datacollection.datasets[0].backgroundColor.push(doc.data().color)
-          this.datacollection.datasets[0].data.push(doc.data().clicks)
+          this.datacollection.datasets[0].data.push(doc.data().hint)
         })
         this.renderChart(this.datacollection, this.options)
       })

f637fe8849bd8535167b30e6e2a8c13116c0344c
arnold-ng <61305922+arnold-ng@users.noreply.github.com>
2020-04-16 13:18:02 +0800

    Update donutjava.vue

--- src/components/Dashboard/donutjava.vue
+++ src/components/Dashboard/donutjava.vue
@@ -1,5 +1,5 @@
 <template>
-  <radial-progress-bar :diameter="205" :completed="com" :total="tot">
+  <radial-progress-bar :diameter="205" :completed-steps="jValue" :total-steps=100>
    <p style="font-family: Futura HV BT">Java Completed: {{ Math.floor(jValue) }}</p>
    
   </radial-progress-bar>
@@ -7,42 +7,18 @@
 
 <script>
 import { mapState } from 'vuex'
-import db from "../../firebase.js";
-import firebase from "firebase"
 import RadialProgressBar from 'vue-radial-progress'
 export default {
     data() {
         return {
-            
-            com:0,
-            totals: 100
-            
+
         }
     },
     components: {
         RadialProgressBar
-       
     },
     methods: {
-        reset() {
-            db.collection('users').doc(this.currentUser.uid).set({
-                
-              
-                jValue: 0
-                
-            }, {merge: true})
-        },
-        
-        
-        updateRank() {
-            if (this.getRank() != this.userProfile.rank) {
-                db.collection('users').doc(this.currentUser.uid).set({
-                    rank: this.getRank(),
-                    prevRank: firebase.firestore.FieldValue.arrayUnion(this.userProfile.rank)
-                }, {merge: true})
-            }
-        },
-        
+
     },
     computed: {
         ...mapState(['userProfile', 'currentUser']),
